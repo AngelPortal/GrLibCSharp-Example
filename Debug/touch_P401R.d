3da34fe5cbc9ad811ac18c3d7876cd437e4a0f1e
AngelPortal <AngelPortal@192.168.1.32>
2016-10-02 21:30:11 -0300

    Carregamento Inicial - Fonte para MSP432. Tarefa: Modificar o fonte para o MSP430.

---
+++ Debug/touch_P401R.d
@@ -0,0 +1,103 @@
+# FIXED
+
+touch_P401R.obj: ../touch_P401R.c
+touch_P401R.obj: C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h
+touch_P401R.obj: C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432p401r.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/msp432p401r_classic.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/system_msp432p401r.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/driverlib.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/adc14.h
+touch_P401R.obj: C:/ti/ccsv6/ccs_base/arm/include/msp.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/aes256.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/comp_e.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/cpu.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/crc32.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/cs.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/dma.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/eusci.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/flash.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/fpu.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/gpio.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/i2c.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/interrupt.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/mpu.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/pcm.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/pmap.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/pss.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/ref_a.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/reset.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/rom.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/rom_map.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/rtc_c.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/spi.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/sysctl.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/systick.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/timer32.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/timer_a.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/uart.h
+touch_P401R.obj: C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/wdt_a.h
+touch_P401R.obj: ../touch_P401R.h
+touch_P401R.obj: C:/Users/AngelPortal/AreaTrabalho/MSP-EXP432P401R_GrLib_Example/GrLib/grlib/grlib.h
+touch_P401R.obj: C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/assert.h
+touch_P401R.obj: C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/linkage.h
+touch_P401R.obj: C:/Users/AngelPortal/AreaTrabalho/MSP-EXP432P401R_GrLib_Example/LcdDriver/LcdDriver/kitronix320x240x16_ssd2119_spi.h
+touch_P401R.obj: C:/Users/AngelPortal/AreaTrabalho/MSP-EXP432P401R_GrLib_Example/GrLib/grlib/grlib.h
+
+../touch_P401R.c: 
+C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/stdint.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmInstr.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmFunc.h: 
+C:/ti/ccsv6/ccs_base/arm/include/CMSIS/core_cmSimd.h: 
+C:/ti/ccsv6/ccs_base/arm/include/system_msp432p401r.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/driverlib.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/adc14.h: 
+C:/ti/ccsv6/ccs_base/arm/include/msp.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/aes256.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/comp_e.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/cpu.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/crc32.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/cs.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/dma.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/eusci.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/flash.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/fpu.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/gpio.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/i2c.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/interrupt.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/mpu.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/pcm.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/pmap.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/pss.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/ref_a.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/reset.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/rom.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/rom_map.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/rtc_c.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/spi.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/sysctl.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/systick.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/timer32.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/timer_a.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/uart.h: 
+C:/ti/MSP432Ware_3_50_00_02/grlib/driverlib/MSP432P4xx/wdt_a.h: 
+../touch_P401R.h: 
+C:/Users/AngelPortal/AreaTrabalho/MSP-EXP432P401R_GrLib_Example/GrLib/grlib/grlib.h: 
+C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/assert.h: 
+C:/ti/ccsv6/tools/compiler/arm_15.12.3.LTS/include/linkage.h: 
+C:/Users/AngelPortal/AreaTrabalho/MSP-EXP432P401R_GrLib_Example/LcdDriver/LcdDriver/kitronix320x240x16_ssd2119_spi.h: 
+C:/Users/AngelPortal/AreaTrabalho/MSP-EXP432P401R_GrLib_Example/GrLib/grlib/grlib.h: 
